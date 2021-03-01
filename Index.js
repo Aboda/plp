70e27bf379ab0f693ca6e647dbb63daf89af6035
Demian <AndThenbeyond@gmail.com>
2021-03-01 23:54:55 +0000

    cleanup pre implementation

--- Index.js
+++
@@ -1,8 +0,0 @@
-exports.servidor = (req, res) => {
-    let message;
-    for (var prop in req) {
-        message = message + " " + prop + ": " + req[prop] + "\n";
-    }
-    message = message + "end of content";
-    res.status(200).send(message);
-  };
