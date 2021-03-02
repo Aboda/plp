0af918a0770cda854840ff639b54817e41ee1534
Demian <AndThenbeyond@gmail.com>
2021-03-02 01:21:14 +0000

    Adapting names for ease of use

--- PublicBucket/Inicio.js
+++
@@ -1,4 +0,0 @@
-window.onload = function () {
-    var no_javascript = document.getElementById("no_javascript");
-    no_javascript.innerText = "Este sitio está construido 100% en los servicios de nube de google cloud y pretende ser.."
-}
