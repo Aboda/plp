c2848adb57e10e51a40b90b8b52db0e1e95a00e5
Demian <AndThenbeyond@gmail.com>
2021-03-04 18:24:20 +0000

    Extracted from server content staging materials

--- infra/bundler.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-
-var html = fs.readFileSync("/home/andthenbeyond/tls/privkey.pem");
-var css = fs.readFileSync("/home/andthenbeyond/tls/privkey.pem");
-var js = fs.readFileSync("/home/andthenbeyond/tls/privkey.pem");
