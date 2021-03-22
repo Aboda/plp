215d50426a91746c553fd0ebf7c944c8465507d7
Demian Sanchez <andthenbeyond@gmail.com>
2021-03-21 20:07:45 -0600

    changed to an object in the reporter

--- computeengine/reporter.js
+++ computeengine/reporter.js
@@ -1,23 +1,23 @@
 exports.spawn = () => {
-    return function report () {
-        var rep_sh = "";
-        var start = (rep) => {
-            rep_sh = rep_sh + "<div>"+rep.service_no+"</div>\n";
-            rep_sh = rep_sh + "<div>"+rep.timestamp+"</div>\n";
-            rep_sh = rep_sh + "<div>"+rep.step+"</div>\n";
-            rep_sh = rep_sh + "<div>"+rep.caller_ip+"</div>\n";
-            rep_sh = rep_sh + "<div>"+rep.languaje+"</div>\n";
-            rep_sh = rep_sh + "<div>"+rep.host+"</div>\n";
-            rep_sh = rep_sh + "<div>"+rep.method+"</div>\n";
-        }
-        var tag = (tag) => {
-            rep_sh = rep_sh + "<div>"+tag+"</div>\n";
-        }
-        var end_by_error = (tagstring,rf) => {
+    return {
+        "rep_sh":"",
+        "start": function (rep) {
+            this.rep_sh = this.rep_sh + "<div>"+rep.service_no+"</div>\n";
+            this.rep_sh = this.rep_sh + "<div>"+rep.timestamp+"</div>\n";
+            this.rep_sh = this.rep_sh + "<div>"+rep.step+"</div>\n";
+            this.rep_sh = this.rep_sh + "<div>"+rep.caller_ip+"</div>\n";
+            this.rep_sh = this.rep_sh + "<div>"+rep.languaje+"</div>\n";
+            this.rep_sh = this.rep_sh + "<div>"+rep.host+"</div>\n";
+            this.rep_sh = this.rep_sh + "<div>"+rep.method+"</div>\n";
+        },
+        "tag": function (tag) {
+            this.rep_sh = this.rep_sh + "<div>"+tag+"</div>\n";
+        },
+        "end_by_error": function (tagstring,rf) {
             return rf.craft({
                 "type":"html",
                 "title":"porter",
-                "html":rep_sh
+                "html":this.rep_sh
             });
         }
     }
