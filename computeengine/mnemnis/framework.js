03630b3a7885087de39eea901a7a0112fcc5d47b
Demian Sanchez <andthenbeyond@gmail.com>
2021-03-27 21:01:45 -0600

    this this this you know it

--- computeengine/mnemnis/framework.js
+++ computeengine/mnemnis/framework.js
@@ -159,7 +159,7 @@ function left_hand_menu(details) {
     toggle_vis(entry);
     crafted_device.append(entry);
   }
-  crafted_device.addEventListener("click",sidemenu_toggle());
+  crafted_device.addEventListener("click",() => {sidemenu_toggle(this)});
 }
 
 function sidemenu_toggle(what) {
