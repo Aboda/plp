eb496bc76d248d5fd56e7e480936c13d29341c69
Demian Sanchez <andthenbeyond@gmail.com>
2021-03-29 21:54:16 -0600

    last of the night

--- computeengine/mnemnis/info.js
+++ computeengine/mnemnis/info.js
@@ -8,24 +8,23 @@ let side_menu = [
             fetch_file("progress",(response)=>{
                 var data = JSON.parse(response);
                 console.log(data)
-                var pre_existing = ost(ao,"focus",{
-                    "mass_kill":()=>{
+                var new_entity = ost(ao,"focus",{
+                    "kill":()=>{
                         for (var item_name in ao.focus) {
                             ao.focus[item_name].kill();
                         }
                     }
                 });
-                if (pre_existing == false) {
-                    ao.focus.mass_kill();
-                    ost(ao,"focus",{
-                        "mass_kill":()=>{
+                if (new_entity != true) {
+                    ao.focus.kill();
+                    ao.focus = {
+                        "kill":()=>{
                             for (var item_name in ao.focus) {
                                 ao.focus[item_name].kill();
                             }
                         }
-                    });
-                }
-                
+                    }
+                };                
                 var container = make_node({
                     "id":"the_guy_who_always_dies",
                     "nodetype":"div"
@@ -33,17 +32,43 @@ let side_menu = [
                 document.body.append(container);
                 for (var entry in data){
                     var card = make_node({
-                        "nodetype":"div"
+                        "nodetype":"div",
+                        "styles":["progress_card"]
                     },ao.focus);
                     container.append(card);
                     
                     var title = make_node({
-                        "nodetype":"h3",
-                        "innerText": entry
+                        "nodetype":"p",
+                        "innerText": "<b>"+entry+"</b>"
                     },ao.focus);
                     card.append(title);
-                }
-                
+
+                    var progress = make_node({
+                        "nodetype":"p",
+                        "innerText": data[entry].meta.priority
+                    },ao.focus);
+                    card.append(progress);
+
+                    for (var route in data[entry]){
+                        var sub_card = make_node({
+                            "nodetype":"div",
+                            "styles":["sub_progress_card"]
+                        },ao.focus);
+                        card.append(sub_card);
+                        var sub_route = make_node({
+                            "nodetype":"p",
+                            "innerText": route
+                        },ao.focus);
+                        sub_card.append(sub_route);
+
+                        var sub_progress = make_node({
+                            "nodetype":"p",
+                            "innerText": data[entry].astra[route].meta.priority
+                        },ao.focus);
+                        sub_card.append(sub_progress);
+                        
+                    };
+                };
             });
         }
     },
