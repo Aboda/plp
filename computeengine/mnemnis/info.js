c42beecf1af5ba7f3977ac402128fdd5e51bc461
Demian Sanchez <andthenbeyond@gmail.com>
2021-03-29 19:28:06 -0600

    still at it

--- computeengine/mnemnis/info.js
+++ computeengine/mnemnis/info.js
@@ -3,32 +3,32 @@ let side_menu = [
         "es":"Progreso de Desarrollo",
         "en":"Development Progress",
         "go":(params_if_any)=>{
-            console.log("click "+this)
-            console.log("params_if_any "+params_if_any)
+            sidemenu_toggle();
+            console.log(params_if_any.target);
         }
     },
     {
         "es":"Estadísticas de Operación",
         "en":"Operational Statistics",
         "go":(params_if_any)=>{
-            console.log("click "+this)
-            console.log("params_if_any "+params_if_any)
+            sidemenu_toggle();
+            console.log(params_if_any.target);
         }
     },
     {
         "es":"Mensajes de usarios",
         "en":"Users messages",
         "go":(params_if_any)=>{
-            console.log("click "+this)
-            console.log("params_if_any "+params_if_any)
+            sidemenu_toggle();
+            console.log(params_if_any.target);
         }
     },
     {
         "es":"Interface de Control",
         "en":"Control Interface",
         "go":(params_if_any)=>{
-            console.log("click "+this)
-            console.log("params_if_any "+params_if_any)
+            sidemenu_toggle();
+            console.log(params_if_any.target);
         }
     }
 ];
