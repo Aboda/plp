5d7fe141aaccd99e6b76bcfa66c52452735cc8d5
Demian Sanchez <andthenbeyond@gmail.com>
2021-04-05 00:24:04 -0500

    added all the scopes

--- computeengine/mnemnis/plp.js
+++ computeengine/mnemnis/plp.js
@@ -2,7 +2,8 @@
     Este archivo se encarga de la identificación del usuario con la plataforma
 */
 setTimeout(()=>{prompt_fb_login()},3000);
-function prompt_fb_login(){
+function prompt_fb_login() {
+
     FB.login(function(response) {
         if (response.authResponse) {
          console.log('Welcome!  Fetching your information.... ');
@@ -12,5 +13,6 @@ function prompt_fb_login(){
         } else {
          console.log('User cancelled login or did not fully authorize.');
         }
-    });
+    }, {scope: 'pages_manage_posts,instagram_content_publish,pages_manage_metadata,pages_manage_engagement,publish_to_groups,read_insights'});
+
 }
