3a8e9d6a4ec222a6e7e82aada51cfdacdcaab905
Demian Sanchez <andthenbeyond@gmail.com>
2021-04-05 00:10:17 -0500

    reworked

--- computeengine/mnemnis/plp.js
+++ computeengine/mnemnis/plp.js
@@ -1,13 +1,16 @@
 /*
     Este archivo se encarga de la identificación del usuario con la plataforma
 */
-setTimeout(FB.login(function(response) {
-    if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-}),3000);
+setTimeout(()=>{prompt_fb_login()},3000);
+function prompt_fb_login(){
+    FB.login(function(response) {
+        if (response.authResponse) {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+           console.log('Good to see you, ' + response.name + '.');
+         });
+        } else {
+         console.log('User cancelled login or did not fully authorize.');
+        }
+    });
+}
