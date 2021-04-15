3fa9fd62e199e12b61baf26b1209a077b916c88d
Demian Sanchez <andthenbeyond@gmail.com>
2021-04-14 21:12:50 -0500

    testing bigger bar, demoved plp from root domain

--- computeengine/crocia.js
+++ computeengine/crocia.js
@@ -41,7 +41,7 @@ exports.set_cache_n_init = (cache) => {
                     "en":"Root:PLP"
                 },
                 "css":[resources_cache.css.sidebar,resources_cache.css.plp],
-                "js":[resources_cache.js.framework,resources_cache.js.demian_app,resources_cache.js.plp]
+                "js":[resources_cache.js.framework,resources_cache.js.demian_app]
             },
             "astra":{
                 "conavi":{
