82c181d822f1630476d842a10a675abea972b7f9
Demian Sanchez <andthenbeyond@gmail.com>
2021-04-18 14:59:00 -0500

    testing again

--- computeengine/realmiu/demian_app_somema.js
+++ computeengine/realmiu/demian_app_somema.js
@@ -94,8 +94,23 @@ function build_google_login_button(){
         "nodetype":"div",
         "styles":["g-signin2"]
     })
-    button.setAttribute("data-onsuccess",OA2_success);
+    button.setAttribute("data-onsuccess","OA2_success");
     return button;
+}
+
+function build_google_logout_button (){
+    let button = ao.qq({
+        "nodetype":"button",
+        "triggers":[["click",signOut]]
+    })
+    return button;
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
 }
 
 function OA2_success(huh) {
