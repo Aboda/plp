e787ff02fcb4046fecfe4903768d01e4e8e475e6
Demian Sanchez <andthenbeyond@gmail.com>
2021-05-24 00:18:24 -0500

    midway changing structure to create image cards

--- realmiu/demian_app_drawio.js
+++ realmiu/demian_app_drawio.js
@@ -1,29 +1,15 @@
 /*
     Página estática con dos idiomas posibles
-*/
-
-let plain_content = [
-    ["h1",
-    "Privacy Policy",
-    "Politica de Privacidad"
-    ],
-    ["p",
-    "This server does not store information regarding its users identity",
-    "Este servidor no guarda información de la identidad de sus usuarios"
-    ],
-    ["p",
-    "Temporal logging with the requester ip address, method and requested resource is taken for site administration purposes.",
-    "Un registro temporal con la dirección ip del solicitante, el método usado y el recurso solicitado es llevado para fines de administración del sitio."
-    ],
-    ["p",
-    "Session management is handled on the web client with the vendors SDKs",
-    "El control de sesiones es manejado en el cliente web con los SDKs proveídos por terceros"
-    ],
-    ["p",
-    "The only cookie used is google analytics which provides anonymized usage data",
-    "La única cookie utilizada es google analytics que provee información de uso anonimizada"
-    ]
+    
+let available_diagrams = [
+    {
+        "element":"thumbnail",
+        "src":"",
+        "alt":
+    }
 ]
+
+*/
 
 window.onload = () => {
     ao.main = document.getElementById("from_home");
