a69daeb70dcb3632fd4ff43dffbf49fead572521
Demian Sanchez <andthenbeyond@gmail.com>
2025-09-10 16:49:47 -0600

    trying to recompact

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const fs = require("fs").promises;
 const https = require("https");
 const suprarouter = require("./utilities/suprarouter.js");
 async function main(){
-    let tls_path = String(await fs.readFile("./din/tls_path.txt"));
-    tls_path = tls_path.trim();
-    console.log(tls_path)
+    const tls_path = String(await fs.readFile("./din/tls_path.txt")).trim();
     const opts = {
         key: await fs.readFile(tls_path +"/privkey.pem"),
         cert: await fs.readFile(tls_path + "/fullchain.pem")
