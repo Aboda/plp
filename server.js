9995c700ac0cdebbe6117eab6b78d5dfa3d310d6
Demian Sanchez <andthenbeyond@gmail.com>
2025-09-10 16:33:43 -0600

    testing path to tls data

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const suprarouter = require("./utilities/suprarouter.js");
 async function main(){
     const tls_path = await fs.readFile("./din/tls_path.txt");
     const opts = {
-        key: await fs.readFile(tls_path +"privkey.pem"),
-        cert: await fs.readFile(tls_path + "fullchain.pem")
+        key: await fs.readFile(tls_path +"/privkey.pem"),
+        cert: await fs.readFile(tls_path + "/fullchain.pem")
     };
     const server = https.createServer(opts,async(req,res)=>{try{suprarouter(req,res)}catch(err){console.error("Serve catch",err)}});
     server.listen(443);
