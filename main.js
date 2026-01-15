a692f09e7d6cf59fffa2ee30e05fd9ffadc68112
Demian Sanchez <andthenbeyond@gmail.com>
2026-01-15 17:47:04 -0600

    expandng time for event since we are about to handle big uploads

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const fs = require("fs").promises;
 const https = require("https");
 const suprarouter = require("./utilities/suprarouter.js");
-async function main(){
-    const tls_path = String(await fs.readFile("./din/tls_path.txt")).trim();
-    const opts = {
-        key: await fs.readFile(tls_path +"/privkey.pem"),
-        cert: await fs.readFile(tls_path + "/fullchain.pem"),
-        maxCachedSessions: 10,
-        keepAliveTimeout: 10000,
-        headersTimeout: 3000,
-        maxHeadersCount: 15,
-        requestTimeout: 120000,
-        timeout:0
-    };
-    const server = https.createServer(opts,async(req,res)=>{try{suprarouter(req,res)}catch(err){console.error("Serve catch",err)}});
-    server.listen(443);
-};
+async function main() {
+  const tls_path = String(await fs.readFile("./din/tls_path.txt")).trim();
+  const opts = {
+    key: await fs.readFile(tls_path + "/privkey.pem"),
+    cert: await fs.readFile(tls_path + "/fullchain.pem"),
+    maxCachedSessions: 10,
+    keepAliveTimeout: 10000,
+    headersTimeout: 3000,
+    maxHeadersCount: 15,
+    requestTimeout: 240000,
+    timeout: 0,
+  };
+  const server = https.createServer(opts, async (req, res) => {
+    try {
+      suprarouter(req, res);
+    } catch (err) {
+      console.error("Serve catch", err);
+    }
+  });
+  server.listen(443);
+}
 main();
